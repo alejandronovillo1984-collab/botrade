52c0662f7c803a5ea8f5b1185ff9e78b1c9533b7
ale <ale@botrade.local>
2026-07-16 08:55:07 -0300

    chore: publish web to main

--- apps/web/src/lib/firebase.ts
+++ apps/web/src/lib/firebase.ts
@@ -1,7 +1,7 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth, Auth } from 'firebase/auth';
+import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFunctions, Functions } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,10 +13,46 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const hasValidConfig = Boolean(firebaseConfig.apiKey && firebaseConfig.appId);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const functions = getFunctions(app);
+let app: FirebaseApp | null = null;
+let _auth: Auth | null = null;
+let _db: Firestore | null = null;
+let _functions: Functions | null = null;
 
-export default app;
+function getAppInstance(): FirebaseApp {
+  if (app) return app;
+  if (getApps().length > 0) {
+    app = getApp();
+  } else if (hasValidConfig) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    throw new Error(
+      'Firebase config missing. Set NEXT_PUBLIC_FIREBASE_* env vars.'
+    );
+  }
+  return app;
+}
+
+export const auth = new Proxy({} as Auth, {
+  get(_t, prop) {
+    if (!_auth) _auth = getAuth(getAppInstance());
+    return Reflect.get(_auth, prop);
+  },
+}) as Auth;
+
+export const db = new Proxy({} as Firestore, {
+  get(_t, prop) {
+    if (!_db) _db = getFirestore(getAppInstance());
+    return Reflect.get(_db, prop);
+  },
+}) as Firestore;
+
+export const functions = new Proxy({} as Functions, {
+  get(_t, prop) {
+    if (!_functions) _functions = getFunctions(getAppInstance());
+    return Reflect.get(_functions, prop);
+  },
+}) as Functions;
+
+export default undefined;
